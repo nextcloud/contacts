85e1a7dfb076832114f6971aa0eba700837679d7
Jan-Christoph Borchardt <hey@jancborchardt.net>
2015-12-10 16:15:23 +0100

    fix name and icon

--- appinfo/app.php
+++ appinfo/app.php
@@ -36,6 +36,6 @@ $container->query('OCP\INavigationManager')->add(function () use ($container) {
 
 		// the title of your application. This will be used in the
 		// navigation or on the settings page of your app
-		'name' => $l10n->t('Contacts Rework'),
+		'name' => $l10n->t('Contacts'),
 	];
 });
