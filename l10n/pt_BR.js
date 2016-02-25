d988d111bd5810a2f1af34338fa2c2b3a17f80a0
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-02-25 01:03:07 -0500

    [tx-robot] updated from transifex

---
+++ l10n/pt_BR.js
@@ -0,0 +1,10 @@
+OC.L10N.register(
+    "contactsrework",
+    {
+    "Contacts" : "Contatos",
+    "No contacts in here" : "Nenhum contato aqui",
+    "Add contact" : "Adicionar contatos",
+    "All contacts" : "Todos os contatos",
+    "{addressbook} shared by {owner}" : "{addressbook} compartilhado por {owner}"
+},
+"nplurals=2; plural=(n > 1);");
---
+++ l10n/pt_BR.php
@@ -0,0 +1,9 @@
+<?php
+$TRANSLATIONS = array(
+"Contacts" => "Contatos",
+"No contacts in here" => "Nenhum contato aqui",
+"Add contact" => "Adicionar contatos",
+"All contacts" => "Todos os contatos",
+"{addressbook} shared by {owner}" => "{addressbook} compartilhado por {owner}"
+);
+$PLURAL_FORMS = "nplurals=2; plural=(n > 1);";
