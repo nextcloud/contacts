8d091e4bfbbe7a3890b4b808de91e8a18bcd436f
Hendrik Leppelsack <hendrik@leppelsack.de>
2016-02-29 01:47:22 +0100

    automatically select first contact of group

--- js/components/contactList/contactList_controller.js
+++ js/components/contactList/contactList_controller.js
@@ -1,10 +1,12 @@
-app.controller('contactlistCtrl', ['$scope', 'ContactService', '$routeParams', function($scope, ContactService, $routeParams) {
+app.controller('contactlistCtrl', ['$scope', '$filter', '$route', '$routeParams', 'ContactService', function($scope, $filter, $route, $routeParams, ContactService) {
 	var ctrl = this;
 
 	ctrl.routeParams = $routeParams;
 	ctrl.t = {
 		addContact : t('contactsrework', 'Add contact')
 	};
+
+	ctrl.contactList = [];
 
 	ContactService.registerObserverCallback(function(contacts) {
 		$scope.$apply(function() {
@@ -16,6 +18,27 @@ app.controller('contactlistCtrl', ['$scope', 'ContactService', '$routeParams', f
 		$scope.$apply(function(){
 			ctrl.contacts = contacts;
 		});
+	});
+
+	$scope.$watch('ctrl.routeParams.uid', function(newValue) {
+		if(newValue === undefined) {
+			// we might have to wait until ng-repeat filled the contactList
+			if(ctrl.contactList.length > 0) {
+				$route.updateParams({
+					gid: $routeParams.gid,
+					uid: ctrl.contactList[0].uid()
+				});
+			} else {
+				// watch for next contactList update
+				var unbindWatch = $scope.$watch('ctrl.contactList', function(newValue) {
+					$route.updateParams({
+						gid: $routeParams.gid,
+						uid: ctrl.contactList[0].uid()
+					});
+					unbindWatch(); // unbind as we only want one update
+				});
+			}
+		}
 	});
 
 	ctrl.createContact = function() {
