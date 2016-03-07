d9bdc42176e4b90aca4e05b434be89818cd87836
Thomas Müller <thomas.mueller@tmit.eu>
2016-03-07 18:18:49 +0100

    Fix initial groups value display

--- js/components/parsers/groupModel_directive.js
+++ js/components/parsers/groupModel_directive.js
@@ -4,6 +4,9 @@ app.directive('groupModel', ['$filter', function($filter){
         require: 'ngModel',
         link: function(scope, element, attr, ngModel) {
             ngModel.$formatters.push(function(value) {
+                if (value.trim().length === 0) {
+                    return [];
+                }
                 return value.split(',');
             });
             ngModel.$parsers.push(function(value) {
