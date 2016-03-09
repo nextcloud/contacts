dff149a63e80dad707312da15af9be9914bdcc41
Hendrik Leppelsack <hendrik@leppelsack.de>
2016-03-09 11:13:16 +0100

    add better js linting

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp'),
 	concat = require('gulp-concat'),
-	jshint = require('gulp-jshint');
+	eslint = require('gulp-eslint');
 
 gulp.task('js', function() {
 	return gulp.src([
@@ -10,11 +10,24 @@ gulp.task('js', function() {
 			'js/services/**/*.js',
 			'js/filters/**/*.js'
 			])
-		.pipe(jshint('.jshintrc'))
-		.pipe(jshint.reporter('default'))
+		.pipe(eslint())
+		.pipe(eslint.format())
 		.pipe(concat('script.js'))
 		.pipe(gulp.dest('js/public'));
 });
+
+gulp.task('eslint', function() {
+	return gulp.src([
+			'js/main.js',
+			'js/components/**/*.js',
+			'js/models/**/*.js',
+			'js/services/**/*.js',
+			'js/filters/**/*.js'
+			])
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failAfterError());
+})
 
 gulp.task('watch', ['js'], function() {
 	gulp.watch('js/**/*.js', ['js']);
