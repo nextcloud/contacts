e6325484fcbde0cd1ca40cb4bd3992717cfff9c6
Thomas Müller <thomas.mueller@tmit.eu>
2016-03-11 23:35:58 +0100

    Parse pref from type

--- js/components/detailsItem/detailsItem_controller.js
+++ js/components/detailsItem/detailsItem_controller.js
@@ -3,6 +3,7 @@ app.controller('detailsItemCtrl', function($templateRequest, vCardPropertiesServ
 
 	ctrl.meta = vCardPropertiesService.getMeta(ctrl.name);
 	ctrl.type = undefined;
+	ctrl.isPreferred = false;
 	ctrl.t = {
 		poBox : t('contacts', 'Post Office Box'),
 		postalCode : t('contacts', 'Postal Code'),
@@ -15,9 +16,25 @@ app.controller('detailsItemCtrl', function($templateRequest, vCardPropertiesServ
 
 	ctrl.availableOptions = ctrl.meta.options || [];
 	if (!_.isUndefined(ctrl.data) && !_.isUndefined(ctrl.data.meta) && !_.isUndefined(ctrl.data.meta.type)) {
-		ctrl.type = ctrl.data.meta.type[0];
-		if (!ctrl.availableOptions.some(function(e) { return e.id === ctrl.data.meta.type[0]; } )) {
-			ctrl.availableOptions = ctrl.availableOptions.concat([{id: ctrl.data.meta.type[0], name: ctrl.data.meta.type[0]}]);
+		// parse type of the property
+		var array = ctrl.data.meta.type[0].split(',');
+		array = array.map(function (elem) {
+			return elem.trim().replace(/\/+$/, '').replace(/\\+$/, '').trim().toUpperCase();
+		});
+		// the pref value is handled on its own so that we can add some favorite icon to the ui if we want
+		if (array.indexOf('PREF') >= 0) {
+			ctrl.isPreferred = true;
+			array.splice(array.indexOf('PREF'), 1);
+		}
+		// simply join the upper cased types together as key
+		ctrl.type = array.join(',');
+		var displayName = array.map(function (element) {
+			return element.charAt(0).toUpperCase() + element.slice(1).toLowerCase();
+		}).join(' ');
+
+		// in case the type is not yet in the default list of available options we add it
+		if (!ctrl.availableOptions.some(function(e) { return e.id === ctrl.type; } )) {
+			ctrl.availableOptions = ctrl.availableOptions.concat([{id: ctrl.type, name: displayName}]);
 		}
 	}
 	ctrl.availableGroups = [];
@@ -27,6 +44,9 @@ app.controller('detailsItemCtrl', function($templateRequest, vCardPropertiesServ
 	});
 
 	ctrl.changeType = function (val) {
+		if (ctrl.isPreferred) {
+			val += ',PREF';
+		}
 		ctrl.data.meta = ctrl.data.meta || {};
 		ctrl.data.meta.type = ctrl.data.meta.type || [];
 		ctrl.data.meta.type[0] = val;
