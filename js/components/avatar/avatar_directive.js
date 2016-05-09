f026e4660679799673fd42b60214f656cb812a32
John Molakvoæ <skjnldsv@users.noreply.github.com>
2016-05-09 10:00:34 +0200

    Add file size limitation and error popup (1MB)

--- js/components/avatar/avatar_directive.js
+++ js/components/avatar/avatar_directive.js
@@ -11,17 +11,21 @@ angular.module('contactsApp')
 			var input = element.find('input');
 			input.bind('change', function() {
 				var file = input.get(0).files[0];
-				var reader = new FileReader();
+				if (file.size > 1024*1024) { // 1 MB
+					OC.Notification.showTemporary(t('contacts', 'The selected image is too big (max 1MB)'));
+				} else {
+					var reader = new FileReader();
 
-				reader.addEventListener('load', function () {
-					scope.$apply(function() {
-						scope.contact.photo(reader.result);
-						ContactService.update(scope.contact);
-					});
-				}, false);
+					reader.addEventListener('load', function () {
+						scope.$apply(function() {
+							scope.contact.photo(reader.result);
+							ContactService.update(scope.contact);
+						});
+					}, false);
 
-				if (file) {
-					reader.readAsDataURL(file);
+					if (file) {
+						reader.readAsDataURL(file);
+					}
 				}
 			});
 		},
