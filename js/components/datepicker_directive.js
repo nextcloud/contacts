8bd3a7aea2037197305daeb260f8e3298ba351c0
Thomas Müller <DeepDiver1975@users.noreply.github.com>
2016-12-15 00:02:58 +0100

    * Allow selected year to be smaller then 1000 (#553)
    * Allow text only input for dates - e.g. ca 1802

--- js/components/datepicker_directive.js
+++ js/components/datepicker_directive.js
@@ -9,7 +9,17 @@ angular.module('contactsApp')
 					dateFormat:'yy-mm-dd',
 					minDate: null,
 					maxDate: null,
-					onSelect:function (date) {
+					constrainInput: false,
+					onSelect:function (date, dp) {
+						if (dp.selectedYear < 1000) {
+							date = '0' + date;
+						}
+						if (dp.selectedYear < 100) {
+							date = '0' + date;
+						}
+						if (dp.selectedYear < 10) {
+							date = '0' + date;
+						}
 						ngModelCtrl.$setViewValue(date);
 						scope.$apply();
 					}
