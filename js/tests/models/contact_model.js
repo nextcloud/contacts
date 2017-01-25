6581c6b119fe4abb52afe8759afe9c48f431b524
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2017-01-25 12:22:03 +0100

    Testing
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- js/tests/models/contact_model.js
+++ js/tests/models/contact_model.js
@@ -22,6 +22,20 @@ describe('contactModel', function() {
 		expect(contact.matches('kenya')).to.equal(true);
 	});
 
+	it('should parse a valid avatar', function() {
+		var contact = new $Contact({displayName: 'test'});
+		var base64Photo = 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQEASABIAAD/2wBDAAMCAgMCAgMDAwMEAwMEBQgFBQQEBQoHBwYIDAoMDAsKCwsNDhIQDQ4RDgsLEBYQERMUFRUVDA8XGBYUGBIUFRT/2wBDAQMEBAUEBQkFBQkUDQsNFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBT/wgARCAAEAAQDAREAAhEBAxEB/8QAFAABAAAAAAAAAAAAAAAAAAAAB//EABUBAQEAAAAAAAAAAAAAAAAAAAQG/9oADAMBAAIQAxAAAAEuPT//xAAVEAEBAAAAAAAAAAAAAAAAAAADBP/aAAgBAQABBQKiplT/xAAYEQACAwAAAAAAAAAAAAAAAAABAwACMf/aAAgBAwEBPwGz2E7P/8QAGBEAAgMAAAAAAAAAAAAAAAAAAQIAAyH/2gAIAQIBAT8BFSLgE//EABcQAQADAAAAAAAAAAAAAAAAAAEAAgP/2gAIAQEABj8CW2is/8QAFRABAQAAAAAAAAAAAAAAAAAAAFH/2gAIAQEAAT8hq3j/2gAMAwEAAgADAAAAEN//xAAWEQADAAAAAAAAAAAAAAAAAAAAARH/2gAIAQMBAT8QumP/xAAVEQEBAAAAAAAAAAAAAAAAAAAAAf/aAAgBAgEBPxCaH//EABgQAQADAQAAAAAAAAAAAAAAAAEAIVFh/9oACAEBAAE/ECIUCwdzVn//2Q==';
+		contact.photo(base64Photo);
+		expect(contact.photo()).to.equal(base64Photo);
+	});
+
+	it('should ignore an invalid avatar', function() {
+		var contact = new $Contact({displayName: 'test'});
+		var base64Photo = 'ENCODING=b:iVBORw0KGgoAAAANSUhEUgAAAHwAAAB8CAMAAACcwCSMAAAAgVBMVEX/0QEAAAD/0QD/1wH/1AH/3QH/2QGkigE/NABPQQBSQwABIAAD/2wBDAAEQEBAAAAAAAAEBAAE/ECIUCwdzVn//2Q==';
+		contact.photo(base64Photo);
+		expect(contact.photo()).to.be.undefined;
+	});
+
 	it('should generate proper ISO.8601.2004 date string', function() {
 		var contact = new $Contact({displayName: 'test'});
 		var d = contact.getISODate(new Date('2016-09-01T09:07:05Z'));
