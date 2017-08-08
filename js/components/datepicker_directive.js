97dddfe856efbc13dd413bfd4ef987e100f5ac12
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2017-08-08 11:43:55 +0200

    Fixed datepicker select route breaker Uncaught Missing instance data for this datepicker
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- js/components/datepicker_directive.js
+++ js/components/datepicker_directive.js
@@ -1,30 +1,32 @@
 angular.module('contactsApp')
-.directive('datepicker', function() {
+.directive('datepicker', function($timeout) {
+	var loadDatepicker = function (scope, element, attrs, ngModelCtrl) {
+		$timeout(function() {
+			element.datepicker({
+				dateFormat:'yy-mm-dd',
+				minDate: null,
+				maxDate: null,
+				constrainInput: false,
+				onSelect:function (date, dp) {
+					if (dp.selectedYear < 1000) {
+						date = '0' + date;
+					}
+					if (dp.selectedYear < 100) {
+						date = '0' + date;
+					}
+					if (dp.selectedYear < 10) {
+						date = '0' + date;
+					}
+					ngModelCtrl.$setViewValue(date);
+					scope.$apply();
+				}
+			});
+		});
+	};
 	return {
 		restrict: 'A',
 		require : 'ngModel',
-		link : function (scope, element, attrs, ngModelCtrl) {
-			$(function() {
-				element.datepicker({
-					dateFormat:'yy-mm-dd',
-					minDate: null,
-					maxDate: null,
-					constrainInput: false,
-					onSelect:function (date, dp) {
-						if (dp.selectedYear < 1000) {
-							date = '0' + date;
-						}
-						if (dp.selectedYear < 100) {
-							date = '0' + date;
-						}
-						if (dp.selectedYear < 10) {
-							date = '0' + date;
-						}
-						ngModelCtrl.$setViewValue(date);
-						scope.$apply();
-					}
-				});
-			});
-		}
+		transclude: true,
+		link : loadDatepicker
 	};
 });
