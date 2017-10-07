b147bf8ed28cc09c9a22bb1f55aa94dbb2e73f36
rakekniven <mark.ziegler@rakekniven.de>
2017-10-07 11:08:12 +0200

    Update import_service.js
    Should be ellipsis → … not tripledot → ...

--- js/services/import_service.js
+++ js/services/import_service.js
@@ -3,12 +3,12 @@ angular.module('contactsApp')
 
 	this.importing = false;
 	this.selectedAddressBook = t('contacts', 'Import into');
-	this.importedUser = t('contacts', 'Waiting for the server to be ready...');
+	this.importedUser = t('contacts', 'Waiting for the server to be ready…');
 	this.importPercent = 0;
 
 	this.t = {
 		importText : t('contacts', 'Import into'),
-		importingText : t('contacts', 'Importing...')
+		importingText : t('contacts', 'Importing…')
 	};
 
 });
