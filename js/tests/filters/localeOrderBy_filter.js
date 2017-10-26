7146194a237d644f1759635654157b2b2f4ecfa4
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2017-10-26 16:24:34 +0200

    Added test and format tests arrays for clarification purpose
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- js/tests/filters/localeOrderBy_filter.js
+++ js/tests/filters/localeOrderBy_filter.js
@@ -10,15 +10,68 @@ describe('localeOrderBy filter', function() {
 
 	it('should return the array properly sorted', function() {
 		var localeOrderBy = $filter('localeOrderBy');
-		var sorted = $filter('localeOrderBy')([{name:'Mb'}, {name:'Mab'}, {name:'Máa'}, {name: 1}, {name: undefined}, {name: undefined}], ['name'], false);
-		var expected = [{name: 1}, {name:'Máa'}, {name:'Mab'}, {name:'Mb'}, {name: undefined}, {name: undefined}];
+		var sorted = $filter('localeOrderBy')(
+			[
+				{name:'Mb'},
+				{name:'Mab'},
+				{name:'Máa'},
+				{name: 1},
+				{name: undefined},
+				{name: undefined}
+			],
+			'name',
+			false
+		);
+		var expected = [
+			{name: 1},
+			{name:'Máa'},
+			{name:'Mab'},
+			{name:'Mb'},
+			{name: undefined},
+			{name: undefined}
+		];
 		expect(sorted).to.deep.equal(expected);
 	});
 
 	it('should return the array properly sorted by uid if the name are all equals', function() {
 		var localeOrderBy = $filter('localeOrderBy');
-		var sorted = $filter('localeOrderBy')([{name:'Test', uid: '4321'}, {name:'Test', uid: '1234'}, {name:'Test2', uid: '0000'}, {name: 1, uid: '5241'}], ['name', 'uid'], false);
-		var expected = [{name: 1, uid: '5241'}, {name:'Test', uid: '1234'}, {name:'Test', uid: '4321'}, {name:'Test2', uid: '0000'}];
+		var sorted = $filter('localeOrderBy')(
+			[
+				{name:'Test', uid: '4321'},
+				{name:'Test', uid: '1234'},
+				{name:'Test2', uid: '0000'},
+				{name: 1, uid: '5241'}
+			],
+			['name', 'uid'],
+			false
+		);
+		var expected = [
+			{name: 1, uid: '5241'},
+			{name:'Test', uid: '1234'},
+			{name:'Test', uid: '4321'},
+			{name:'Test2', uid: '0000'}
+		];
+		expect(sorted).to.deep.equal(expected);
+	});
+
+	it('should return the array properly sorted by uid even if both first and last name are equals', function() {
+		var localeOrderBy = $filter('localeOrderBy');
+		var sorted = $filter('localeOrderBy')(
+			[
+				{name:['New contact', 'New contact'], uid: '3456'},
+				{name:['New contact', 'New contact'], uid: '1234'},
+				{name:['New contact', 'New contact'], uid: '2345'},
+				{name:['New contact', 'New contact'], uid: '0123'}
+			],
+			['name', 'uid'],
+			false
+		);
+		var expected = [
+			{name:['New contact', 'New contact'], uid: '0123'},
+			{name:['New contact', 'New contact'], uid: '1234'},
+			{name:['New contact', 'New contact'], uid: '2345'},
+			{name:['New contact', 'New contact'], uid: '3456'}
+		];
 		expect(sorted).to.deep.equal(expected);
 	});
 });
