386bb10929c104b3c10b7efccb0ac7bfa1069c00
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2017-10-26 20:53:37 +0200

    Don't invert by default for readability purpose
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- js/filters/localeOrderBy_filter.js
+++ js/filters/localeOrderBy_filter.js
@@ -29,23 +29,23 @@ angular.module('contactsApp')
 				// Start sorting
 				if (angular.isString(valueA)) {
 					if(valueA !== valueB) {
-						return !reverseOrder ? valueA.localeCompare(valueB) : valueB.localeCompare(valueA);
+						return reverseOrder ? valueB.localeCompare(valueA) : valueA.localeCompare(valueB);
 					}
 				}
 
 				if (angular.isNumber(valueA) || typeof valueA === 'boolean') {
 					if(valueA !== valueB) {
-						return !reverseOrder ? valueA - valueB : valueB - valueA;
+						return reverseOrder ? valueB - valueA : valueA - valueB;
 					}
 				}
 
 				if (angular.isArray(valueA)) {
 					if (valueA[0] === valueB[0]) {
 						if (valueA[1] !== valueB[1]) {
-							return !reverseOrder ? valueA[1].localeCompare(valueB[1]) : valueB[1].localeCompare(valueA[1]);
+							return reverseOrder ? valueB[1].localeCompare(valueA[1]) :valueA[1].localeCompare(valueB[1]);
 						}
 					} else {
-						return !reverseOrder ? valueA[0].localeCompare(valueB[0]) : valueB[0].localeCompare(valueA[0]);
+						return reverseOrder ? valueB[0].localeCompare(valueA[0]) : valueA[0].localeCompare(valueB[0]);
 					}
 				}
 			}
