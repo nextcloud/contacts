238fa4a5f57f42bf4e1fa24a595975578eab4a34
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2018-02-27 18:06:58 +0100

    Fixed tests
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- js/tests/parsers/urlModel_directive.js
+++ js/tests/parsers/urlModel_directive.js
@@ -10,9 +10,9 @@ describe('Unit testing url model', function() {
 	// so they are available to all tests in this describe block
 	beforeEach(inject(function(_$compile_, _$rootScope_){
 		// The injector unwraps the underscores (_) from around the parameter names when matching
-		$compile = _$compile_;
-		$rootScope = _$rootScope_;
-		$scope = $rootScope;
+		var $compile = _$compile_;
+		var $rootScope = _$rootScope_;
+		var $scope = $rootScope.$new();
 		var element = angular.element(
 			'<form name="form"><input url-model ng-model="model.url" name="url" /></form>'
 		);
@@ -22,44 +22,67 @@ describe('Unit testing url model', function() {
 		form = $scope.form;
 	}));
 
-	it('Add http protocol', function() {
+	it('Add http protocol with $parsers', function() {
 		form.url.$setViewValue('parsers.nextcloud.com');
 		expect($scope.model.url).to.equal('https://parsers.nextcloud.com');
+	});
 
+	it('Add https protocol with $formatters', function() {
 		$scope.model.url = 'formatters.nextcloud.com';
+		$scope.$digest();
 		expect(form.url.$modelValue).to.equal('https://formatters.nextcloud.com');
 	});
-	it('Don\'t add http protocol', function() {
+
+	it('Don\'t add http protocol with $parsers', function() {
 		form.url.$setViewValue('http://parsers.nextcloud.com');
 		expect($scope.model.url).to.equal('http://parsers.nextcloud.com');
+
 		form.url.$setViewValue('https://parsers.nextcloud.com');
 		expect($scope.model.url).to.equal('https://parsers.nextcloud.com');
 
+	});
+
+	it('Don\'t add http protocol with $formatters', function() {
 		$scope.model.url = 'http://formatters.nextcloud.com';
+		$scope.$digest();
 		expect(form.url.$modelValue).to.equal('http://formatters.nextcloud.com');
+
 		$scope.model.url = 'https://formatters.nextcloud.com';
+		$scope.$digest();
 		expect(form.url.$modelValue).to.equal('https://formatters.nextcloud.com');
 	});
-	it('should recognize different protocols, apart from http(s)', function() {
+
+	it('should recognize different protocols, apart from http(s) with $parsers', function() {
 		form.url.$setViewValue('ftp://parsers.nextcloud.com');
 		expect($scope.model.url).to.equal('ftp://parsers.nextcloud.com');
+	});
 
+	it('should recognize different protocols, apart from http(s) with $formatters', function() {
 		$scope.model.url = 'ftp://formatters.nextcloud.com';
+		$scope.$digest();
 		expect(form.url.$modelValue).to.equal('ftp://formatters.nextcloud.com');
 	});
-	it('should recognize protocols without using double slashes', function() {
+
+	it('should recognize protocols without using double slashes with $parsers', function() {
 		form.url.$setViewValue('ftp:parsers.nextcloud.com');
 		expect($scope.model.url).to.equal('ftp://parsers.nextcloud.com');
 		form.url.$setViewValue('http:parsers.nextcloud.com');
 		expect($scope.model.url).to.equal('http://parsers.nextcloud.com');
 		form.url.$setViewValue('https:parsers.nextcloud.com');
 		expect($scope.model.url).to.equal('https://parsers.nextcloud.com');
+	});
 
+	it('should recognize protocols without using double slashes with $formatters', function() {
 		$scope.model.url = 'ftp:formatters.nextcloud.com';
+		$scope.$digest();
 		expect(form.url.$modelValue).to.equal('ftp://formatters.nextcloud.com');
-		$scope.model.url = 'ftp:formatters.nextcloud.com';
+
+		$scope.model.url = 'http:formatters.nextcloud.com';
+		$scope.$digest();
 		expect(form.url.$modelValue).to.equal('http://formatters.nextcloud.com');
-		$scope.model.url = 'ftp:formatters.nextcloud.com';
+
+		$scope.model.url = 'https:formatters.nextcloud.com';
+		$scope.$digest();
 		expect(form.url.$modelValue).to.equal('https://formatters.nextcloud.com');
 	});
 });
