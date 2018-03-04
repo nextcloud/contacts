537e2916bbe8638fa4a9c5a318a74a5bccde8018
Jessica Greene <jessica0greene@gmail.com>
2018-03-04 17:09:05 +0100

    Fix issue 445 to increase range of number of contacts shown.

--- js/tests/filters/counterFormatter_filter.js
+++ js/tests/filters/counterFormatter_filter.js
@@ -9,13 +9,13 @@ describe('counterFormatter filter', function() {
 		$filter = _$filter_;
 	}));
 
-	it('should return the same number or 999+ if greater than 999', function() {
+	it('should return the same number or 9999+ if greater than 9999', function() {
 		var counterFormatter = $filter('counterFormatter');
 		expect(counterFormatter(Number.NaN)).to.be.Nan;
 		expect(counterFormatter(15)).to.equal(15);
 		expect(counterFormatter(0)).to.equal('');
 		expect(counterFormatter(-5)).to.equal(-5);
-		expect(counterFormatter(999)).to.equal(999);
-		expect(counterFormatter(1000)).to.equal('999+');
+		expect(counterFormatter(9999)).to.equal(9999);
+		expect(counterFormatter(10000)).to.equal('9999+');
 	});
 });
