2334527f1f10b84d2c6e6d05981d9dc7c7fb9255
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2018-08-01 11:36:05 +0200

    Accessibility on list
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- src/components/contentList/contentListItem.vue
+++ src/components/contentList/contentListItem.vue
@@ -1,14 +1,16 @@
 <template>
-	<a :class="{active: selectedContact === contact.key}" href="" class="app-content-list-item"
-		@click.prevent.stop="selectContact">
-		<input ref="selected" :id="contact.uid" type="checkbox"
-			class="app-content-list-item-checkbox checkbox">
-		<label :for="contact.uid" @click.prevent.stop="toggleSelect" />
+	<div :class="{active: selectedContact === contact.key}" tabindex="0" class="app-content-list-item"
+		@click.prevent.stop="selectContact" @keypress.enter.prevent.stop="selectContact">
+		<!-- keyboard accessibility will focus the input and not the label -->
+		<input ref="selected" :id="contact.key" type="checkbox"
+			class="app-content-list-item-checkbox checkbox" @keypress.enter.space.prevent.stop="toggleSelect">
+		<label :for="contact.key" @click.prevent.stop="toggleSelect" @keypress.enter.space.prevent.stop="toggleSelect" />
 		<div :style="{ 'backgroundColor': colorAvatar }" class="app-content-list-item-icon">{{ contact.displayName | firstLetter }}</div>
 		<div class="app-content-list-item-line-one">{{ contact.displayName }}</div>
 		<div v-if="contact.email" class="app-content-list-item-line-two">{{ contact.email }}</div>
-		<div v-if="contact.addressbook.enabled" class="icon-delete" @click.prevent.stop="deleteContact" />
-	</a>
+		<div v-if="contact.addressbook.enabled" class="icon-delete" tabindex="0"
+			@click.prevent.stop="deleteContact" @keypress.enter.prevent.stop="deleteContact" />
+	</div>
 </template>
 
 <script>
