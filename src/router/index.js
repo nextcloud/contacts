9bde32fcee808eb5be130449d440a2608de3df16
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2018-11-14 12:54:22 +0100

    Fix custom app support
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- src/router/index.js
+++ src/router/index.js
@@ -22,7 +22,7 @@
 
 import Vue from 'vue'
 import Router from 'vue-router'
-import { generateFilePath } from 'nextcloud-server/dist/router'
+import { generateUrl } from 'nextcloud-server/dist/router'
 import Contacts from 'Views/Contacts'
 
 Vue.use(Router)
@@ -31,7 +31,7 @@ export default new Router({
 	mode: 'history',
 	// if index.php is in the url AND we got this far, then it's working:
 	// let's keep using index.php in the url
-	base: generateFilePath('contacts', '', ''),
+	base: generateUrl('/apps/contacts', ''),
 	linkActiveClass: 'active',
 	routes: [
 		{
