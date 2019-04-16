92c383ecc27bdef1ecab288f972d4abaf6d666f0
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2019-04-16 10:46:56 +0200

    Proper IE warning
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- templates/main.php
+++ templates/main.php
@@ -1,4 +1,15 @@
 <?php
-script('contacts', 'contacts');
-style('contacts', 'contacts');
+if (!\OCP\Util::isIe()) {
+	script('contacts', 'contacts');
+	style('contacts', 'contacts');
+} else {
 ?>
+<div id="app-content">
+	<div class="emptycontent">
+		<div class="icon-contacts-dark"></div>
+		<h2><?php p($l->t('Your web browser is out of date')); ?></h2>
+		<p><?php p($l->t('This application is not compatible with Internet Explorer')); ?></p>
+	</div>
+</div>
+
+<?php } ?>
