e88af8c7df14540681215ec2d58e20830734162f
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2020-02-25 08:36:17 +0100

    Rollback deps upgrade & upgrade eslint config
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- tests/setup.js
+++ tests/setup.js
@@ -19,14 +19,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-require('jsdom-global')()
-global.expect = require('chai').expect
-
 // https://github.com/vuejs/vue-test-utils/issues/936
 // better fix for "TypeError: Super expression must either be null or
 // a function" than pinning an old version of prettier.
-//
-// https://github.com/vuejs/vue-cli/issues/2128#issuecomment-453109575
 window.Date = Date
 
 global.OC = {
--- webpack.common.js
+++ webpack.common.js
@@ -12,43 +12,43 @@ module.exports = {
 		path: path.resolve(__dirname, './js'),
 		publicPath: '/js/',
 		filename: `${appName}.js`,
-		chunkFilename: 'chunks/[name]-[hash].js'
+		chunkFilename: 'chunks/[name]-[hash].js',
 	},
 	module: {
 		rules: [
 			{
 				test: /\.css$/,
-				use: ['vue-style-loader', 'css-loader']
+				use: ['vue-style-loader', 'css-loader'],
 			},
 			{
 				test: /\.scss$/,
-				use: ['vue-style-loader', 'css-loader', 'sass-loader']
+				use: ['vue-style-loader', 'css-loader', 'sass-loader'],
 			},
 			{
 				test: /\.(js|vue)$/,
 				use: 'eslint-loader',
 				exclude: /node_modules/,
-				enforce: 'pre'
+				enforce: 'pre',
 			},
 			{
 				test: /\.vue$/,
 				loader: 'vue-loader',
-				exclude: /node_modules/
+				exclude: /node_modules/,
 			},
 			{
 				test: /\.js$/,
 				loader: 'babel-loader',
-				exclude: /node_modules/
-			}
-		]
+				exclude: /node_modules/,
+			},
+		],
 	},
 	plugins: [
 		new VueLoaderPlugin(),
 		new StyleLintPlugin(),
 		new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-		new webpack.DefinePlugin({ appVersion })
+		new webpack.DefinePlugin({ appVersion }),
 	],
 	resolve: {
-		extensions: ['*', '.js', '.vue']
-	}
+		extensions: ['*', '.js', '.vue'],
+	},
 }
