04c79650b06773cb840ba4cc8836f7915037bb87
Valdnet <47037905+Valdnet@users.noreply.github.com>
2020-12-05 20:20:55 +0100

    Change to the name of the app
    It should refer to the application name "Files".

--- src/components/ContactDetails/ContactDetailsAvatar.vue
+++ src/components/ContactDetails/ContactDetailsAvatar.vue
@@ -60,7 +60,7 @@
 					<ActionButton
 						icon="icon-folder"
 						@click="selectFilePicker">
-						{{ t('contacts', 'Choose from files') }}
+						{{ t('contacts', 'Choose from Files') }}
 					</ActionButton>
 					<ActionButton
 						v-for="network in supportedSocial"
