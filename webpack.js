ce9182f3130996265456c6e275d66267c846624d
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2021-03-29 11:00:56 +0200

    Bump dependencies and webpack 5
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- webpack.js
+++ webpack.js
@@ -4,6 +4,8 @@ const webpackConfig = require('@nextcloud/webpack-vue-config')
 
 webpackConfig.entry['files-action'] = path.join(__dirname, 'src', 'files-action.js')
 webpackConfig.entry['admin-settings'] = path.join(__dirname, 'src', 'admin-settings.js')
-webpackConfig.plugins.push(new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/))
+webpackConfig.plugins.push(...[
+	new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+])
 
 module.exports = webpackConfig
