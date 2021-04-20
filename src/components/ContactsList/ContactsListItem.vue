c948ec1aa78690e9a4902d66fc1365d469cc4565
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2021-04-20 17:04:27 +0200

    Contacts list to ListItemIcon
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- src/components/ContactsList/ContactsListItem.vue
+++ src/components/ContactsList/ContactsListItem.vue
@@ -1,43 +1,28 @@
 <template>
-	<div :id="id"
-		:class="{active: selectedContact === contact.key}"
+	<ListItemIcon
+		:id="id"
+		:key="source.key"
+		:avatar-size="44"
+		:class="{'contacts-list__item--active': selectedContact === source.key}"
+		:display-name="source.displayName"
+		:is-no-user="true"
+		:subtitle="source.email"
+		:title="source.displayName"
+		:url="avatarUrl"
+		class="contacts-list__item"
 		tabindex="0"
-		class="app-content-list-item"
 		@click.prevent.stop="selectContact"
-		@keypress.enter.prevent.stop="selectContact">
-		<!-- keyboard accessibility will focus the input and not the label -->
-		<!--
-		<input ref="selected" :id="contact.key" type="checkbox"
-			class="app-content-list-item-checkbox checkbox" @keypress.enter.space.prevent.stop="toggleSelect">
-		<label :for="contact.key" @click.prevent.stop="toggleSelect" @keypress.enter.space.prevent.stop="toggleSelect" />
-		-->
-		<Avatar
-			:disable-menu="true"
-			:disable-tooltip="true"
-			:display-name="contact.displayName"
-			:is-no-user="true"
-			:size="40"
-			:url="avatarUrl"
-			class="app-content-list-item-icon" />
-
-		<!-- contact data -->
-		<div class="app-content-list-item-line-one">
-			{{ contact.displayName }}
-		</div>
-		<div v-if="contact.email" class="app-content-list-item-line-two">
-			{{ contact.email }}
-		</div>
-	</div>
+		@keypress.enter.prevent.stop="selectContact" />
 </template>
 
 <script>
-import Avatar from '@nextcloud/vue/dist/Components/Avatar'
+import ListItemIcon from '@nextcloud/vue/dist/Components/ListItemIcon'
 
 export default {
 	name: 'ContactsListItem',
 
 	components: {
-		Avatar,
+		ListItemIcon,
 	},
 
 	props: {
@@ -45,7 +30,7 @@ export default {
 			type: Number,
 			required: true,
 		},
-		contact: {
+		source: {
 			type: Object,
 			required: true,
 		},
@@ -61,36 +46,43 @@ export default {
 
 		// usable and valid html id for scrollTo
 		id() {
-			return window.btoa(this.contact.key).slice(0, -2)
+			return window.btoa(this.source.key).slice(0, -2)
 		},
 
 		avatarUrl() {
-			if (this.contact.photo) {
-				return `${this.contact.photoUrl}`
+			if (this.source.photo) {
+				return `${this.source.photoUrl}`
 			}
-			if (this.contact.url) {
-				return `${this.contact.url}?photo`
+			if (this.source.url) {
+				return `${this.source.url}?photo`
 			}
 			return undefined
 		},
 	},
 	methods: {
-		/**
-		 * Checkbox management method
-		 */
-		toggleSelect() {
-			// toggle checkbox here because we stop the propagation to not trigger selectContact
-			// therefore the selectContact prevent the checkbox label+input propagation
-			this.$refs.selected.checked = !this.$refs.selected.checked
-		},
 
 		/**
 		 * Select this contact within the list
 		 */
 		selectContact() {
 			// change url with router
-			this.$router.push({ name: 'contact', params: { selectedGroup: this.selectedGroup, selectedContact: this.contact.key } })
+			this.$router.push({ name: 'contact', params: { selectedGroup: this.selectedGroup, selectedContact: this.source.key } })
 		},
 	},
 }
 </script>
+<style lang="scss" scoped>
+.contacts-list__item {
+	padding: 8px;
+
+	&--active,
+	&:focus,
+	&:hover {
+		background-color: var(--color-background-hover);
+	}
+
+	&, ::v-deep * {
+		cursor: pointer;
+	}
+}
+</style>
