5fec07abf282c01ede1a5fcbf2eff89367b39c62
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2021-05-03 19:13:32 +0200

    4.0.0-alpha.0
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,3 @@
-module.exports = {
-	plugins: [
-		'@babel/plugin-syntax-dynamic-import',
-		['@babel/plugin-proposal-class-properties', { loose: true }],
-	],
-	presets: [
-		[
-			'@babel/preset-env',
-			{
-				corejs: 3,
-				useBuiltIns: 'entry',
-			},
-		],
-	],
-}
+const babelConfig = require('@nextcloud/babel-config')
+
+module.exports = babelConfig
