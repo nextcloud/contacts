35042be1954030c846be40dfdce1c2854b539a18
Anna Larch <anna@nextcloud.com>
2022-01-03 15:41:52 +0100

    Remove OC_Util::isIe()
    Signed-off-by: Anna Larch <anna@nextcloud.com>

--- templates/main.php
+++ templates/main.php
@@ -1,13 +1,1 @@
 <?php
-if (\OC_Util::isIe()) {
-	?>
-<div id="app-content">
-	<div class="emptycontent">
-		<div class="icon-contacts-dark"></div>
-		<h2><?php p($l->t('Your web browser is out of date')); ?></h2>
-		<p><?php p($l->t('This application is not compatible with Internet Explorer')); ?></p>
-	</div>
-</div>
-
-<?php
-} ?>
