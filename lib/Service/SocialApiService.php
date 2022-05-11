14c76935d6e459c5f3a79ce53c31f4916a8961c7
Anna Larch <anna@nextcloud.com>
2022-05-11 10:52:15 +0200

    Add check for empty search result
    Signed-off-by: Anna Larch <anna@nextcloud.com>

--- lib/Service/SocialApiService.php
+++ lib/Service/SocialApiService.php
@@ -180,11 +180,13 @@ class SocialApiService {
 			}
 
 			// search contact in that addressbook, get social data
-			$contact = $addressBook->search($contactId, ['UID'], ['types' => true])[0];
+			$contacts = $addressBook->search($contactId, ['UID'], ['types' => true]);
 
-			if (!isset($contact)) {
+			if (empty($contacts)) {
 				return new JSONResponse([], Http::STATUS_PRECONDITION_FAILED);
 			}
+
+			$contact = $contacts[0];
 
 			if ($network) {
 				$allConnectors = [$this->socialProvider->getSocialConnector($network)];
