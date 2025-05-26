6804b5ebb2cdef6858de75253cbb4d53c81a5264
Grigory Vodyanov <scratchx@gmx.com>
2025-05-26 21:52:01 +0200

    feat(ContactsList): add multiselect action and batch deleting
    Signed-off-by: Grigory Vodyanov <scratchx@gmx.com>

--- src/components/ContactsList.vue
+++ src/components/ContactsList.vue
@@ -5,25 +5,73 @@
 
 <template>
 	<AppContentList class="content-list">
+		<NcDialog :open="showDeleteConfirmationDialog"
+			:name="n(
+				'contacts',
+				'Delete {number} contact',
+				'Delete {number} contacts',
+				multiSelectedContacts.size,
+				{ number: multiSelectedContacts.size }
+			)"
+			:buttons="buttons"
+			no-close>
+			{{ t('contacts', 'Are you sure you want to proceed?') }}
+			<span v-if="readOnlyMultiSelectedCount" class="content-list-dialog-warning">
+				{{ n('contacts',
+					'Please note that {number} contact is read only and won\'t be deleted',
+					'Please note that {number} contacts are read only and won\'t be deleted',
+					readOnlyMultiSelectedCount,
+					{ number: readOnlyMultiSelectedCount })
+				}}
+			</span>
+		</NcDialog>
+
 		<div class="contacts-list__header">
 			<div class="search-contacts-field">
 				<input v-model="query" type="text" :placeholder="t('contacts', 'Search contacts …')">
 			</div>
 		</div>
+		<div v-if="isMultiSelecting" class="contacts-list__multiselect-header">
+			<NcButton type="tertiary"
+				:title="n('contacts', 'Unselect {number}', 'Unselect {number}', multiSelectedContacts.size, { number: multiSelectedContacts.size })"
+				:close-after-click="true"
+				@click.prevent="unselectAllMultiSelected">
+				<IconSelect :size="16" />
+			</NcButton>
+			<NcButton type="tertiary"
+				:title="n(
+					'contacts',
+					'Delete {number} contact',
+					'Delete {number} contacts',
+					multiSelectedContacts.size,
+					{ number: multiSelectedContacts.size }
+				)"
+				:close-after-click="true"
+				@click.prevent="attemptDeleteAllMultiSelected">
+				<IconDelete :size="16" />
+			</NcButton>
+		</div>
+
 		<VirtualList ref="scroller"
 			class="contacts-list"
 			data-key="key"
 			:data-sources="filteredList"
 			:data-component="ContactsListItem"
 			:estimate-size="68"
-			:extra-props="{reloadBus}" />
+			:extra-props="{reloadBus, onSelectMultipleFromParent: onSelectMultiple, onSelectRangeFromParent: onSelectRange }" />
 	</AppContentList>
 </template>
 
 <script>
-import { NcAppContentList as AppContentList } from '@nextcloud/vue'
+import { NcAppContentList as AppContentList, NcButton, NcDialog } from '@nextcloud/vue'
 import ContactsListItem from './ContactsList/ContactsListItem.vue'
 import VirtualList from 'vue-virtual-scroll-list'
+import IconSelect from 'vue-material-design-icons/CloseThick.vue'
+import IconDelete from 'vue-material-design-icons/Delete.vue'
+// eslint-disable-next-line import/no-unresolved
+import IconCancelRaw from '@mdi/svg/svg/cancel.svg?raw'
+// eslint-disable-next-line import/no-unresolved
+import IconDeleteRaw from '@mdi/svg/svg/delete.svg?raw'
 
 export default {
 	name: 'ContactsList',
@@ -31,6 +79,10 @@ export default {
 	components: {
 		AppContentList,
 		VirtualList,
+		NcButton,
+		IconSelect,
+		IconDelete,
+		NcDialog,
 	},
 
 	props: {
@@ -56,6 +108,23 @@ export default {
 		return {
 			ContactsListItem,
 			query: '',
+			multiSelectedContacts: new Map(),
+			refreshKey: 0, // used to force re-render of the list when search query changes, can be removed in vue3
+			showDeleteConfirmationDialog: false,
+			buttons: [
+				{
+					label: t('contacts', 'Cancel'),
+					icon: IconCancelRaw,
+					callback: () => { this.showDeleteConfirmationDialog = false },
+				},
+				{
+					label: t('contacts', 'Delete'),
+					type: 'primary',
+					icon: IconDeleteRaw,
+					callback: () => { this.deleteAllMultiSelected() },
+				},
+			],
+			lastToggledIndex: undefined,
 		}
 	},
 
@@ -67,9 +136,29 @@ export default {
 			return this.$route.params.selectedGroup
 		},
 		filteredList() {
-			return this.list
+			const contactsList = this.list
 				.filter(item => this.matchSearch(this.contacts[item.key]))
 				.map(item => this.contacts[item.key])
+
+			contactsList.forEach((contact, index) => {
+				contact.isMultiSelected = this.multiSelectedContacts.has(index)
+			})
+
+			return contactsList
+		},
+		isMultiSelecting() {
+			return this.multiSelectedContacts.size > 0
+		},
+		readOnlyMultiSelectedCount() {
+			let count = 0
+
+			this.multiSelectedContacts.forEach((contact) => {
+				if (contact.addressbook.readOnly) {
+					count++
+				}
+			})
+
+			return count
 		},
 	},
 
@@ -145,6 +234,70 @@ export default {
 			}
 			return true
 		},
+
+		onSelectMultiple(contact, index, isRange = false) {
+			if (isRange && this.lastToggledIndex !== index) {
+				if (this.onSelectRange(index)) {
+					return
+				}
+			}
+
+			if (this.multiSelectedContacts.has(index)) {
+				this.multiSelectedContacts.delete(index)
+			} else {
+				this.multiSelectedContacts.set(index, contact)
+			}
+			this.lastToggledIndex = index
+			this.$set(this, 'multiSelectedContacts', new Map(this.multiSelectedContacts))
+		},
+
+		onSelectRange(index) {
+			const lastToggledIndex = this.lastToggledIndex ?? undefined
+			if (lastToggledIndex === undefined) {
+				return false
+			}
+
+			const start = Math.min(lastToggledIndex, index)
+			const end = Math.max(lastToggledIndex, index)
+			const selected = this.multiSelectedContacts.has(index)
+
+			const newSelection = new Map(this.multiSelectedContacts)
+
+			for (let i = start; i <= end; i++) {
+				if (!selected) {
+					newSelection.set(i, this.filteredList[i])
+				} else {
+					newSelection.delete(i)
+				}
+			}
+
+			this.lastToggledIndex = index
+			this.$set(this, 'multiSelectedContacts', newSelection)
+
+			return true
+		},
+
+		unselectAllMultiSelected() {
+			this.$set(this, 'multiSelectedContacts', new Map())
+			this.lastToggledIndex = undefined
+		},
+
+		attemptDeleteAllMultiSelected() {
+			this.showDeleteConfirmationDialog = true
+		},
+
+		deleteAllMultiSelected() {
+			this.multiSelectedContacts.forEach(async (contact) => {
+				if (contact.addressbook.readOnly) {
+					// Do not try to delete read only contacts
+					return
+				}
+				await new Promise(resolve => setTimeout(resolve, 500))
+				await this.$store.dispatch('deleteContact', { contact })
+			})
+			this.unselectAllMultiSelected()
+			this.showDeleteConfirmationDialog = false
+		},
 	},
 }
 </script>
@@ -175,4 +328,20 @@ export default {
 	padding: 0 var(--default-grid-baseline);
 }
 
+.contacts-list__multiselect-header {
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: center;
+	background-color: var(--color-main-background-translucent);
+	position: sticky;
+	height: calc(var(--default-grid-baseline) * 12);
+	z-index: 100;
+}
+
+.content-list-dialog-warning {
+	display: block;
+	margin-top: var(--default-grid-baseline);
+	color: var(--color-text-lighter);
+}
 </style>
