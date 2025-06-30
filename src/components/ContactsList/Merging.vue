2e452eb50aa94ac3fc04825c58b74448eb72e17a
Grigory Vodyanov <scratchx@gmx.com>
2025-06-30 22:49:02 +0200

    fixup! feat(ContactsList): add contact merging action

--- src/components/ContactsList/Merging.vue
+++ src/components/ContactsList/Merging.vue
@@ -5,48 +5,69 @@
 
 <template>
 	<div class="merging">
-		<span>{{ t('contacts', 'Please resolve the following contacts') }}</span>
+		<div class="merging-title">
+			<h3>{{ t('contacts', 'Merged contacts summary') }}</h3>
+			<NcButton :disabled="conflictsToResolve !== 0" variant="secondary">
+				{{ t('contacts', 'Merge contacts') }}
+				<template #icon>
+					<IconSetMerge :size="16" />
+				</template>
+			</NcButton>
+		</div>
 
-		{{ contacts }}
+		<NcNoteCard v-if="conflictsToResolve"
+			type="warning"
+			:text="n('contacts', 'You have {x} conflict to resolve', 'You have {x} conflicts to resolve', conflictsToResolve, { x: conflictsToResolve })" />
 
 		<div class="merging-conflicts">
-			<div v-for="(properties, name) in firstElementsInDiff"
-				:key="name">
-				<ContactDetailsProperty v-for="(property, index) in properties"
-					:key="`${index}-${contactsList[0].key}-${property.name}`"
-					:is-first-property="index===0"
-					:is-last-property="index === properties.length - 1"
-					:property="property"
-					:contact="contactsList[0]"
-					:local-contact="contactsList[0]"
-					:contacts="contacts"
-					:bus="bus"
-					:is-read-only="true" />
-			</div>
-			<div v-for="(properties, name) in secondElementsInDiff"
-				:key="name">
-				<ContactDetailsProperty v-for="(property, index) in properties"
-					:key="`${index}-${contactsList[0].key}-${property.name}`"
-					:is-first-property="index===0"
-					:is-last-property="index === properties.length - 1"
-					:property="property"
-					:contact="contactsList[0]"
-					:local-contact="contactsList[0]"
-					:contacts="contacts"
-					:bus="bus"
-					:is-read-only="true" />
+			<div v-for="(property) in usedProperties"
+				:key="property"
+				:class="['merging-conflicts__row', { conflict: conflictInformation[property]?.type === 'conflict' }]">
+				<IconCheckCircleOutline v-if="conflictInformation[property]?.type !== 'conflict'" :size="20" />
+				<IconCloseCircleOutline v-if="conflictInformation[property]?.type === 'conflict'" :size="20" :class="[{ 'needs-action': resolvedConflicts.get(property) === undefined}]" />
+
+				<NcCheckboxRadioSwitch v-if="conflictInformation[property]?.type === 'conflict'"
+					:checked="resolvedConflicts.get(property) === 0"
+					@update:checked="resolveConflict(0, property)" />
+				<div v-if="dividedProperties[0][property]"
+					:class="['merging-conflicts__property', { 'no-conflict': conflictInformation[property]?.type !== 'conflict' }]">
+					<ContactDetailsProperty :is-first-property="true"
+						:is-last-property="false"
+						:property="dividedProperties[0][property]"
+						:contact="contactsList[0]"
+						:local-contact="contactsList[0]"
+						:contacts="contacts"
+						:bus="bus"
+						:is-read-only="true" />
+				</div>
+				<div v-if="!dividedProperties[0][property]" class="merging-conflicts__filler" />
+
+				<NcCheckboxRadioSwitch v-if="conflictInformation[property]?.type === 'conflict'"
+					:checked="resolvedConflicts.get(property) === 1"
+					@update:checked="resolveConflict(1, property)" />
+				<div v-if="dividedProperties[1][property]"
+					:class="['merging-conflicts__property', { 'no-conflict': conflictInformation[property]?.type !== 'conflict' }]">
+					<ContactDetailsProperty :is-first-property="true"
+						:is-last-property="false"
+						:property="dividedProperties[1][property]"
+						:contact="contactsList[1]"
+						:local-contact="contactsList[1]"
+						:contacts="contacts"
+						:bus="bus"
+						:is-read-only="true" />
+				</div>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-import { NcAppContentList as AppContentList, NcButton, NcDialog, NcNoteCard } from '@nextcloud/vue'
+import { NcAppContentList as AppContentList, NcButton, NcDialog, NcNoteCard, NcCheckboxRadioSwitch } from '@nextcloud/vue'
 
-// eslint-disable-next-line import/no-unresolved
-import IconCancelRaw from '@mdi/svg/svg/cancel.svg?raw'
-// eslint-disable-next-line import/no-unresolved
-import IconDeleteRaw from '@mdi/svg/svg/delete.svg?raw'
+import IconCheckCircleOutline from 'vue-material-design-icons/CheckCircleOutline.vue'
+import IconCloseCircleOutline from 'vue-material-design-icons/CloseCircleOutline.vue'
+import IconSetMerge from 'vue-material-design-icons/SetMerge.vue'
+
 import ContactDetailsProperty from '../ContactDetails/ContactDetailsProperty.vue'
 import rfcProps from '../../models/rfcProps.js'
 import mitt from 'mitt'
@@ -56,7 +77,12 @@ export default {
 
 	components: {
 		ContactDetailsProperty,
-
+		NcCheckboxRadioSwitch,
+		IconCheckCircleOutline,
+		IconCloseCircleOutline,
+		IconSetMerge,
+		NcNoteCard,
+		NcButton,
 	},
 
 	props: {
@@ -69,6 +95,8 @@ export default {
 	data() {
 		return {
 			bus: mitt(),
+			resolvedConflicts: new Map(),
+			conflictsToResolve: 0,
 		}
 	},
 
@@ -77,13 +105,7 @@ export default {
 			return Array.from(this.contacts.values()).map(contact => (contact))
 		},
 
-		/**
-		 * Compares the grouped properties of the first two contacts in contactsList
-		 * and returns an object containing only the differences.
-		 *
-		 * @return {object}
-		 */
-		difference() {
+		dividedProperties() {
 			const [contactA, contactB] = this.contactsList
 			if (!contactA || !contactB) return {}
 
@@ -93,47 +115,60 @@ export default {
 			const groupedA = this.groupedProperties(sortedA)
 			const groupedB = this.groupedProperties(sortedB)
 
-			const diff = {}
+			Object.keys(groupedA).forEach(key => {
+				groupedA[key] = groupedA[key][0]
+			})
 
-			const allKeys = new Set([
-				...Object.keys(groupedA),
-				...Object.keys(groupedB),
-			])
+			Object.keys(groupedB).forEach(key => {
+				groupedB[key] = groupedB[key][0]
+			})
 
-			for (const key of allKeys) {
-				const propsA = groupedA[key] || []
-				const propsB = groupedB[key] || []
-
-				const setA = new Set(propsA.map(p => JSON.stringify(p)))
-				const setB = new Set(propsB.map(p => JSON.stringify(p)))
-
-				const onlyInA = propsA.filter(p => !setB.has(JSON.stringify(p)))
-				const onlyInB = propsB.filter(p => !setA.has(JSON.stringify(p)))
-
-				if (onlyInA.length || onlyInB.length) {
-					diff[key] = [...onlyInA, ...onlyInB]
-				}
-			}
-
-			return diff
+			return [groupedA, groupedB]
 		},
-		firstElementsInDiff() {
-			const result = {}
-			for (const key in this.difference) {
-				if (Array.isArray(this.difference[key]) && this.difference[key].length > 0) {
-					result[key] = this.difference[key][0]
-				}
-			}
-			return result
+
+		usedProperties() {
+			const allKeys = this.dividedProperties.flatMap(map => Object.keys(map))
+			return [...new Set(allKeys)]
 		},
-		secondElementsInDiff() {
-			const result = {}
-			for (const key in this.difference) {
-				if (Array.isArray(this.difference[key]) && this.difference[key].length > 1) {
-					result[key] = this.difference[key][1]
+
+		conflictInformation() {
+			const conflictInformation = {}
+
+			this.usedProperties.forEach(property => {
+				if (!this.dividedProperties[0][property]) {
+					conflictInformation[property] = {
+						type: 'onlyInSecond',
+						value: this.dividedProperties[1][property],
+					}
+
+					return
 				}
-			}
-			return result
+
+				if (!this.dividedProperties[1][property]) {
+					conflictInformation[property] = {
+						type: 'onlyInFirst',
+						value: this.dividedProperties[0][property],
+					}
+
+					return
+				}
+
+				if (this.getPropertyValue(this.dividedProperties[0][property]) === this.getPropertyValue(this.dividedProperties[1][property])) {
+					conflictInformation[property] = {
+						type: 'equal',
+						value: this.dividedProperties[0][property],
+					}
+
+					return
+				}
+
+				conflictInformation[property] = {
+					type: 'conflict',
+					value: null,
+				}
+			})
+
+			return conflictInformation
 		},
 	},
 
@@ -142,10 +177,21 @@ export default {
 	},
 
 	mounted() {
-
+		this.calculateConflictsToResolve()
 	},
 
 	methods: {
+		resolveConflict(version, property) {
+			if (version === this.resolvedConflicts.get(property)) {
+				// If the version is already resolved, toggle it back to undefined
+				this.resolvedConflicts.set(property, undefined)
+			} else {
+				// Otherwise, set the version as resolved
+				this.resolvedConflicts.set(property, version)
+			}
+			this.$forceUpdate()
+			this.calculateConflictsToResolve()
+		},
 		/**
 		 * Should display the property
 		 *
@@ -211,6 +257,28 @@ export default {
 					return list
 				}, {})
 		},
+
+		calculateConflictsToResolve() {
+			let conflictsCount = 0
+
+			this.usedProperties.forEach(property => {
+				if (this.conflictInformation[property]?.type === 'conflict' && this.resolvedConflicts.get(property) === undefined) {
+					conflictsCount++
+				}
+			})
+
+			this.conflictsToResolve = conflictsCount
+		},
+
+		getPropertyValue(property) {
+			if (property.isMultiValue) {
+				// differences between values types :x;x;x;x;x and x,x,x,x,x
+				return property.isStructuredValue
+					? property.getValues()[0]
+					: property.getValues()
+			}
+			return property.getFirstValue()
+		}
 	},
 }
 </script>
@@ -219,11 +287,71 @@ export default {
 .merging {
 	display: flex;
 	flex-direction: column;
-	margin: calc(var(--default-grid-baseline) * 4);
+	margin: calc(var(--default-grid-baseline) * 8);
+
+	&-title {
+		display: flex;
+		justify-content: space-between;
+		width: 100%;
+		margin: calc(var(--default-grid-baseline) * 3) 0;
+
+		h3 {
+			margin: 0;
+		}
+	}
 
 	&-conflicts {
 		display: flex;
+		flex-direction: column;
 		gap: calc(var(--default-grid-baseline) * 2);
+		margin: auto;
+
+		&__row {
+			display: flex;
+			gap: calc(var(--default-grid-baseline) * 5);
+			align-items: start;
+
+			.material-design-icon {
+				align-self: start;
+				margin-top: calc(var(--default-grid-baseline) * 4 + 2px);
+			}
+
+			.needs-action {
+				color: var(--color-warning-text);
+			}
+
+			.no-conflict {
+				margin-left: 50px;
+			}
+		}
+
+		&__property {
+			display: flex;
+			gap: calc(var(--default-grid-baseline) * 2);
+			min-width: 300px;
+			max-width: 300px;
+		}
+
+		&__filler {
+			min-width: 350px;
+		}
 	}
+
+	.property__label {
+		width: unset;
+	}
+
+	.checkbox-radio-switch {
+		margin-top: calc(var(--default-grid-baseline) * 2);
+	}
+}
+
+// todo remove
+.modal-container__content {
+	overflow: hidden !important;
+}
+
+h2 {
+	margin: 0 !important;
 }
 </style>
