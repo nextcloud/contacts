08569fe9e9634cda3f4a3149c1ed1894f9d33ab7
Richard Steinmetz <richard@steinmetz.cloud>
2025-07-25 10:25:52 +0200

    test: add a basic playwright e2e test
    Signed-off-by: Richard Steinmetz <richard@steinmetz.cloud>

--- jest.config.cjs
+++ jest.config.cjs
@@ -24,4 +24,5 @@ module.exports = {
 		'^.+\\.(css|styl|less|sass|scss|jpg|jpeg|png|svg|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|avif)$': 'jest-transform-stub',
 	},
 	transformIgnorePatterns: [`/node_modules/(?!${esModules})/.+\\.js$`],
+	testPathIgnorePatterns: ['tests/javascript/e2e'],
 }
