4d0bb51eb5fc1380a034fd1136c7b0b1ee7cb46a
nfebe <fenn25.fn@gmail.com>
2025-08-08 12:27:48 +0100

    fix(teams): Restore member management in new ui
    Signed-off-by: nfebe <fenn25.fn@gmail.com>

--- src/components/MemberList/MemberGridItem.vue
+++ src/components/MemberList/MemberGridItem.vue
@@ -10,7 +10,7 @@
 			:is-no-user="true"
 			:size="32">
 			<template #icon>
-				<IconAccountGroup :size="20" />
+				<IconAccountGroupOutline :size="20" />
 			</template>
 		</NcAvatar>
 		<NcAvatar v-else
@@ -18,19 +18,76 @@
 			:display-name="member.displayName"
 			:size="32" />
 		<span class="member-name">{{ member.displayName }}</span>
+		<NcActions v-if="!isTeam">
+			<NcActionText v-if="loading" icon="icon-loading-small">
+				{{ t('contacts', 'Loading …') }}
+			</NcActionText>
+
+			<!-- Normal menu -->
+			<template v-else>
+				<!-- Level picker -->
+				<template v-if="canChangeLevel">
+					<NcActionText>
+						{{ t('contacts', 'Manage level') }}
+						<template #icon>
+							<IconShieldCheckOutline :size="16" />
+						</template>
+					</NcActionText>
+					<NcActionButton v-for="level in availableLevelsChange"
+						:key="level"
+						icon=""
+						@click="changeLevel(level)">
+						{{ levelChangeLabel(level) }}
+					</NcActionButton>
+
+					<NcActionSeparator />
+				</template>
+
+				<!-- Leave or delete member from circle -->
+				<NcActionButton v-if="isCurrentUser && !circle.isOwner" @click="deleteMember">
+					{{ t('contacts', 'Leave team') }}
+					<template #icon>
+						<IconExitToApp :size="16" />
+					</template>
+				</NcActionButton>
+				<NcActionButton v-else-if="canDelete" @click="deleteMember">
+					<template #icon>
+						<IconDeleteOutline :size="20" />
+					</template>
+					{{ t('contacts', 'Remove member') }}
+				</NcActionButton>
+			</template>
+		</NcActions>
 	</div>
 </template>
 
 <script>
-import { NcAvatar } from '@nextcloud/vue'
-import IconAccountGroup from 'vue-material-design-icons/AccountGroupOutline.vue'
+import { CIRCLES_MEMBER_LEVELS, MemberLevels, MemberLevel } from '../../models/constants.ts'
+import Circle from '../../models/circle.ts'
+import { NcAvatar, NcActions, NcActionButton, NcActionSeparator, NcActionText } from '@nextcloud/vue'
+import IconAccountGroupOutline from 'vue-material-design-icons/AccountGroupOutline.vue'
+import IconDeleteOutline from 'vue-material-design-icons/DeleteOutline.vue'
+import IconExitToApp from 'vue-material-design-icons/ExitToApp.vue'
+import IconShieldCheckOutline from 'vue-material-design-icons/ShieldCheckOutline.vue'
+
+import { changeMemberLevel } from '../../services/circles.ts'
+import { showError, DialogBuilder } from '@nextcloud/dialogs'
+import RouterMixin from '../../mixins/RouterMixin.js'
 
 export default {
 	name: 'MemberGridItem',
 	components: {
 		NcAvatar,
-		IconAccountGroup,
+		IconAccountGroupOutline,
+		NcActions,
+		NcActionButton,
+		NcActionSeparator,
+		NcActionText,
+		IconDeleteOutline,
+		IconExitToApp,
+		IconShieldCheckOutline,
 	},
+	mixins: [RouterMixin],
 	props: {
 		member: {
 			type: Object,
@@ -39,6 +96,211 @@ export default {
 		isTeam: {
 			type: Boolean,
 			default: false,
+		},
+	},
+
+	data() {
+		return {
+			loading: false,
+		}
+	},
+
+	computed: {
+		/**
+		 * Return the current circle
+		 *
+		 * @return {Circle}
+		 */
+		circle() {
+			return this.$store.getters.getCircle(this.selectedCircle)
+		},
+
+		/**
+		 * Current user member level
+		 *
+		 * @return {number}
+		 */
+		currentUserLevel() {
+			return this.circle?.initiator?.level || MemberLevels.MEMBER
+		},
+
+		/**
+		 * Current user member level
+		 *
+		 * @return {string}
+		 */
+		currentUserId() {
+			return this.circle?.initiator?.singleId
+		},
+
+		/**
+		 * Available levels change to the current user
+		 *
+		 * @return {Array}
+		 */
+		availableLevelsChange() {
+			const levels = []
+			// Can't change level of owner
+			if (this.member.level === MemberLevels.OWNER) {
+				return levels
+			}
+
+			// Can't change level of yourself
+			if (this.isCurrentUser) {
+				return levels
+			}
+
+			// From MODERATOR, you can set ADMIN
+			if (this.currentUserLevel >= MemberLevels.MODERATOR && this.member.level !== MemberLevels.ADMIN) {
+				levels.push(MemberLevels.ADMIN)
+			}
+
+			// From ADMIN, you can set MODERATOR and MEMBER
+			if (this.currentUserLevel >= MemberLevels.ADMIN) {
+				if (this.member.level !== MemberLevels.MODERATOR) {
+					levels.push(MemberLevels.MODERATOR)
+				}
+				if (this.member.level !== MemberLevels.MEMBER) {
+					levels.push(MemberLevels.MEMBER)
+				}
+			}
+
+			return levels
+		},
+
+		/**
+		 * Is the current member the current user?
+		 *
+		 * @return {boolean}
+		 */
+		isCurrentUser() {
+			return this.member.id === this.currentUserId
+		},
+
+		/**
+		 * Can the current user change the level of others?
+		 *
+		 * @return {boolean}
+		 */
+		canChangeLevel() {
+			return this.circle.canManageMembers
+				&& this.availableLevelsChange.length > 0
+				&& !this.isCurrentUser
+		},
+
+		/**
+		 * Can the current user delete members or?
+		 *
+		 * @return {boolean}
+		 */
+		canDelete() {
+			return this.circle.canManageMembers
+				&& this.member.level <= this.currentUserLevel
+				&& !this.isCurrentUser
+		},
+	},
+	methods: {
+		/**
+		 * Return the promote/demote member action label
+		 *
+		 * @param {MemberLevel} level the member level
+		 * @return {string}
+		 */
+		levelChangeLabel(level) {
+			if (level === MemberLevels.OWNER) {
+				return t('contacts', 'Promote as sole owner')
+			}
+
+			if (this.member.level < level) {
+				return t('contacts', 'Promote to {level}', { level: CIRCLES_MEMBER_LEVELS[level] })
+			}
+			return t('contacts', 'Demote to {level}', { level: CIRCLES_MEMBER_LEVELS[level] })
+		},
+
+		/**
+		 * Delete the current member
+		 */
+		async deleteMember() {
+			if (this.isCurrentUser) {
+				try {
+					const dialog = new DialogBuilder()
+						.setName(t('contacts', 'Leave team'))
+						.setText(t('contacts', 'Are you sure you want to leave this team? This action cannot be undone.'))
+						.setButtons([
+							{
+								label: t('contacts', 'Cancel'),
+								type: 'secondary',
+								callback: () => { /* do nothing, just close */ },
+							},
+							{
+								label: t('contacts', 'Leave team'),
+								type: 'error',
+								callback: async () => {
+									try {
+										await this.doDeleteMember()
+									} catch (e) {
+										this.logger.error('Error in delete member callback', { e })
+										showError(t('contacts', 'Leave team failed.'))
+									}
+								},
+							},
+						])
+						.build()
+
+					await dialog.show()
+				} catch (error) {
+					// User cancelled the dialog - no action needed
+				}
+			} else {
+				await this.doDeleteMember()
+			}
+		},
+
+		async doDeleteMember() {
+			this.loading = true
+
+			try {
+				await this.$store.dispatch('deleteMemberFromCircle', {
+					member: this.member,
+					leave: this.isCurrentUser,
+				})
+			} catch (error) {
+				if (error?.response?.status === 404) {
+					this.logger.debug('Member is not in circle')
+					return
+				}
+				this.logger.error('Could not delete the member', { member: this.member, error })
+				showError(t('contacts', 'Could not delete the member {displayName}', this.member))
+			} finally {
+				this.loading = false
+			}
+		},
+
+		async changeLevel(level) {
+			this.loading = true
+
+			try {
+				await changeMemberLevel(this.circle.id, this.member.id, level)
+				this.showLevelMenu = false
+
+				// If we changed an owner, let's refresh the whole dataset to update all ownership & memberships
+				if (level === MemberLevels.OWNER) {
+					await this.$store.dispatch('getCircle', this.circle.id)
+					await this.$store.dispatch('getCircleMembers', this.circle.id)
+					return
+				}
+
+				// this.member is a class. We're modifying the class setter, not the prop itself
+				// eslint-disable-next-line vue/no-mutating-props
+				this.member.level = level
+			} catch (error) {
+				this.logger.error('Could not change the member level', { level: CIRCLES_MEMBER_LEVELS[level], error })
+				showError(t('contacts', 'Could not change the member level to {level}', {
+					level: CIRCLES_MEMBER_LEVELS[level],
+				}))
+			} finally {
+				this.loading = false
+			}
 		},
 	},
 }
