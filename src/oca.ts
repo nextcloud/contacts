84233f85a1cea696eca05fcc55b6bc184505337c
Richard Steinmetz <richard@steinmetz.cloud>
2025-09-22 09:58:38 +0200

    fix: restore breaking changes of OCA.Contacts.mountContactDetails
    Signed-off-by: Richard Steinmetz <richard@steinmetz.cloud>

---
+++ src/oca.ts
@@ -0,0 +1,60 @@
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+// eslint-disable-next-line import/no-unresolved, n/no-missing-import
+import 'vite/modulepreload-polyfill'
+
+// Global scss sheets
+import './css/contacts.scss'
+
+// Dialogs css
+import '@nextcloud/dialogs/style.css'
+
+import { createApp } from 'vue'
+import ReadOnlyContactDetails from './views/ReadOnlyContactDetails.vue'
+import { createPinia } from 'pinia'
+import store from './store/index.js'
+import LegacyGlobalMixin from './mixins/LegacyGlobalMixin.js'
+
+declare global {
+	interface Window {
+		OCA: {
+			Contacts?: {
+				/**
+				 * Mount the contact details component at the given DOM element.
+				 *
+				 * @param el Html element to mount the component at.
+				 * @param contactEmailAddress Email address of the contact whose details to display.
+				 * @return Component handle with a basic API to control it.
+				 */
+				mountContactDetails(
+					el: HTMLElement,
+					contactEmailAddress: string,
+				): Promise<{ $destroy(): void }>,
+			},
+		},
+	}
+}
+
+window.OCA ??= {}
+window.OCA.Contacts = {
+	async mountContactDetails(el, contactEmailAddress) {
+		const app = createApp(ReadOnlyContactDetails, {
+			contactEmailAddress,
+		})
+
+		const pinia = createPinia()
+		app.use(pinia)
+		app.use(store)
+
+		app.mixin(LegacyGlobalMixin)
+
+		app.mount(el)
+
+		return {
+			$destroy: () => app.unmount(),
+		}
+	},
+}
--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default createAppConfig({
 	'main': path.join(__dirname, 'src', 'main.js'),
 	'files-action': path.join(__dirname, 'src', 'files-action.js'),
 	'admin-settings': path.join(__dirname, 'src', 'admin-settings.js'),
-	'oca': path.join(__dirname, 'src', 'oca.js'),
+	'oca': path.join(__dirname, 'src', 'oca.ts'),
 }, {
 	inlineCSS: false,
 })
