04cef57489fad86ac006d4d3a861d5a0d377ce6b
Kostiantyn Miakshyn <molodchick@gmail.com>
2025-11-28 16:55:05 +0100

    feat: Create invitation links
    Signed-off-by: Kostiantyn Miakshyn <molodchick@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default createAppConfig({
 	'main': path.join(__dirname, 'src', 'main.js'),
 	'files-action': path.join(__dirname, 'src', 'files-action.js'),
 	'admin-settings': path.join(__dirname, 'src', 'admin-settings.js'),
+	'join-invitation': path.join(__dirname, 'src', 'join-invitation.js'),
 	'oca': path.join(__dirname, 'src', 'oca.ts'),
 }, {
 	inlineCSS: false,
