bd369724fdee213269a2279c5606ebb0942160d8
Hamza <hamzamahjoubi221@gmail.com>
2026-01-06 11:20:29 +0100

    fix(orgchart): revive node highlighting
    Signed-off-by: Hamza <hamzamahjoubi221@gmail.com>

--- src/components/OrgChart.vue
+++ src/components/OrgChart.vue
@@ -150,9 +150,9 @@ export default {
 					}
 				})
 				.onNodeClick(d => {
-					if (!this.chartReference.data().filter(item => item.nodeId === d)[0]._upToTheRootHighlighted) {
+					if (!this.chartReference.data().filter(item => item.nodeId === d.id)[0]?._upToTheRootHighlighted) {
 						this.chartReference.clearHighlighting()
-						this.chartReference.setUpToTheRootHighlighted(d).render()
+						this.chartReference.setUpToTheRootHighlighted(d.id).render()
 					} else {
 						this.chartReference.clearHighlighting()
 					}
