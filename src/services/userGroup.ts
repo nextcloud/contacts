137b0210d1249da5e3bfd3732d827a3738d38626
Hamza <hamzamahjoubi221@gmail.com>
2026-03-10 18:09:02 +0100

    fix: async return
    Signed-off-by: Hamza <hamzamahjoubi221@gmail.com>

--- src/services/userGroup.ts
+++ src/services/userGroup.ts
@@ -6,12 +6,12 @@
 import axios from '@nextcloud/axios'
 import { generateOcsUrl } from '@nextcloud/router'
 
-export async function getUserGroups(userId: string): string[] {
+export async function getUserGroups(userId: string): Promise<string[]> {
 	const response = await axios.get(generateOcsUrl('/cloud/users/{userId}/groups/details', { userId }))
 	return response.data.ocs.data.groups
 }
 
-export async function getUserGroupMembers(groupId: string): string[] {
+export async function getUserGroupMembers(groupId: string): Promise<string[]> {
 	const response = await axios.get(generateOcsUrl('/cloud/groups/{groupId}/users', { groupId }))
 	return response.data.ocs.data.users
 }
