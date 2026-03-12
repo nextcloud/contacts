f0ed0c46beb31a80e83b80a26822ffb91513414a
v3DJG6GL <72495210+v3DJG6GL@users.noreply.github.com>
2026-03-12 20:16:54 +0100

    fix(contacts): include REV in MinimalContactProperties for accurate last-modified sorting
    Signed-off-by: v3DJG6GL <72495210+v3DJG6GL@users.noreply.github.com>

--- src/models/contact.js
+++ src/models/contact.js
@@ -25,7 +25,7 @@ function isEmpty(value) {
 export const ContactKindProperties = ['KIND', 'X-ADDRESSBOOKSERVER-KIND']
 
 export const MinimalContactProperties = [
-	'EMAIL', 'UID', 'TEL', 'CATEGORIES', 'FN', 'ORG', 'N', 'X-PHONETIC-FIRST-NAME', 'X-PHONETIC-LAST-NAME', 'X-MANAGERSNAME', 'TITLE', 'NOTE', 'RELATED',
+	'EMAIL', 'UID', 'TEL', 'CATEGORIES', 'FN', 'ORG', 'N', 'X-PHONETIC-FIRST-NAME', 'X-PHONETIC-LAST-NAME', 'X-MANAGERSNAME', 'TITLE', 'NOTE', 'RELATED', 'REV',
 ].concat(ContactKindProperties)
 
 export default class Contact {
