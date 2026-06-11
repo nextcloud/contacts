41b2540ed3342ba0d4e2347036c982b824af9b51
pallavibakale <pallavibakale09@gmail.com>
2026-06-11 11:36:34 -0400

    fix: linting errors
    Signed-off-by: pallavibakale <pallavibakale09@gmail.com>

--- src/mixins/RouterMixin.js
+++ src/mixins/RouterMixin.js
@@ -3,28 +3,28 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import { ROUTE_CIRCLE, ROUTE_USER_GROUP } from '../models/constants.ts';
+import { ROUTE_CIRCLE, ROUTE_USER_GROUP } from '../models/constants.ts'
 
 export default {
-  computed: {
-    // router variables
-    selectedContact() {
-      return this.$route.params.selectedContact;
-    },
-    selectedGroup() {
-      const { name } = this.$route;
-      if (name === 'circle') return ROUTE_CIRCLE;
-      if (name === 'user_group') return ROUTE_USER_GROUP;
-      return this.$route.params.selectedGroup;
-    },
-    selectedCircle() {
-      return this.$route.params.selectedCircle;
-    },
-    selectedUserGroup() {
-      return this.$route.params.selectedUserGroup;
-    },
-    selectedChart() {
-      return this.$route.params.selectedChart;
-    },
-  },
-};
+	computed: {
+		// router variables
+		selectedContact() {
+			return this.$route.params.selectedContact
+		},
+		selectedGroup() {
+			const { name } = this.$route
+			if (name === 'circle') { return ROUTE_CIRCLE }
+			if (name === 'user_group') { return ROUTE_USER_GROUP }
+			return this.$route.params.selectedGroup
+		},
+		selectedCircle() {
+			return this.$route.params.selectedCircle
+		},
+		selectedUserGroup() {
+			return this.$route.params.selectedUserGroup
+		},
+		selectedChart() {
+			return this.$route.params.selectedChart
+		},
+	},
+}
